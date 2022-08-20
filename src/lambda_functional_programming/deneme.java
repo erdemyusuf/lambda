8c61373fbf75d240754e1fbe22351560e9103637
Yusuf Erdem <byerdemnew@gmail.com>
2022-08-20 14:57:16 +0300

    branch 2

--- src/lambda_functional_programming/deneme.java
+++ src/lambda_functional_programming/deneme.java
@@ -3,5 +3,6 @@ package lambda_functional_programming;
 public class deneme {
     public static void main(String[] args) {
         System.out.println("branch deneme");
+        System.out.println("branch 2.deneme");
     }
 }
