7f471def2fad6c9db6dff277413820d52a1ffe5d
Yusuf Erdem <byerdemnew@gmail.com>
2022-08-20 14:56:11 +0300

    master deneme

--- src/lambda_functional_programming/deneme.java
+++ src/lambda_functional_programming/deneme.java
@@ -2,6 +2,11 @@ package lambda_functional_programming;
 
 public class deneme {
     public static void main(String[] args) {
+
         System.out.println("branch deneme");
+        System.out.println("master deneme");
+
     }
+
+
 }
