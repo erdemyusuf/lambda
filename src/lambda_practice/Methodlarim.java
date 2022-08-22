a17c4c56e52a2a82be95ae8290b997d5fdf83c65
Yusuf Erdem <byerdemnew@gmail.com>
2022-08-22 18:50:23 +0300

    practice 1

---
+++ src/lambda_practice/Methodlarim.java
@@ -0,0 +1,8 @@
+package lambda_practice;
+
+public class Methodlarim {//seed (tohum) methodlar create edildi
+    public static boolean ciftElemaniBul(int x){
+        return  x % 2==0;
+        // 2.yol return x % 2!=1;
+    }
+}
---
+++ src/lambda_practice/Runner01.java
@@ -0,0 +1,18 @@
+package lambda_practice;
+
+
+public class Runner01 extends Lambda01{//inherite birazdan
+
+
+    public static void main(String[] args) {
+
+        // aHarfleriSilenGerisiniYazdiran(names);
+        System.out.println("**************");
+        //  maxElemanBul(numbers);
+        System.out.println("**************");
+        //  elemanlariTopla(numbers);
+        System.out.println("**************");
+        elemanlariCarp(numbers);
+
+    }
+}
